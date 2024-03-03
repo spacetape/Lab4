9b38668e5f6f7481b4eff87cb65a96a8d041c98b
spacetape <sapargali.alimkhan@gmail.com>
2024-03-03 23:13:59 +0600

    Initial Commit

---
+++ Lab4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab4
+//
+//  Created by Алимхан Сапаргали on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
